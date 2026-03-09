039faa24a2e3c196951cceeeee3e927d6ecb7a89
Yarik Erofeev <checkmailperm@gmail.com>
2026-03-09 14:02:14 +0500

    Обновлен .js

--- lab5/backend/server.js
+++ lab5/backend/server.js
@@ -1,25 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-const pool = require("./db");
+const app = require("./app");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/rooms", async (req, res) => {
-  try {
-
-    const result = await pool.query("SELECT * FROM rooms");
-
-    res.json(result.rows);
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Database error");
-  }
-});
-
-app.listen(8080, () => {
+app.listen(8080, "0.0.0.0", () => {
   console.log("Server started on port 8080");
 });
