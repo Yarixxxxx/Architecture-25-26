c76dfe1685f14c257161da316ae54c9df2a8d7b0
Yarik Erofeev <checkmailperm@gmail.com>
2026-03-09 12:01:01 +0500

    Добавлено взаимодействие backend и db

--- lab5/backend/server.js
+++ lab5/backend/server.js
@@ -1,5 +1,25 @@
-const app = require("./app/app");
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
 
-app.listen(8080, "0.0.0.0", () => {
-  console.log("Backend running on port 8080");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/rooms", async (req, res) => {
+  try {
+
+    const result = await pool.query("SELECT * FROM rooms");
+
+    res.json(result.rows);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Database error");
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
 });
